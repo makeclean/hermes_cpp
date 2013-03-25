106bf0969880d9737331d5dcc7af947c35e588fe
makeclean <davisa@engr.wisc.edu>
2013-03-25 09:24:05 -0500

    second change to file, loop through command args

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 int main ( int argc, char *argv[])
 {
+  // parse the command line arguments
+  for ( int argnum = 0 ; argnum <= argc ; argnum++ )
+    {
+      std::cout << argv[argnum] << std::endl;
+    }
 
   return 0;
 }
