72b9612c58b990aba469f565a55854f0eaa206e2
makeclean <davisa@engr.wisc.edu>
2013-03-25 10:50:37 -0500

    added error condition when reading file

--- mesh_funcs.cpp
+++ mesh_funcs.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 
 #include "MeshData.hpp"
+
+using namespace std;
+
+/*
+  Function to load the abaqus mesh file pointed to by meshfile
+  On failure it will return the value of 1 to main
+ */
 
 int LoadMeshFile(std::string meshfile, std::vector<node_struct> &node_data
 		 ,std::vector<tet_struct> &tet_data)
 {
+  std::ifstream mesh_file; // the abaqus mesh
+  std::string line_read; // the line being read from file
+
+  std::cout << "Reading abaqus mesh " << meshfile << std::endl;
+  
+  mesh_file.open(meshfile.c_str(), ios::in); // open the mesh file
+  
+  if( mesh_file.is_open() )
+    {
+      // do stuff
+    }
+  else
+    {
+      return 1;
+    }
 
 
-  return 1;
+  return 0;
 }
