20f5a42c8aa112193644b15e08c0e3c5330997d0
makeclean <davisa@engr.wisc.edu>
2013-03-25 10:40:42 -0500

    added first version of mesh_funcs.cpp

---
+++ mesh_funcs.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "MeshData.hpp"
+
+int LoadMeshFile(std::string meshfile, std::vector<node_struct> &node_data
+		 ,std::vector<tet_struct> &tet_data)
+{
+
+
+  return 1;
+}
