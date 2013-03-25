3ec784051fe82a942845201801a221b4511fa62a
makeclean <davisa@engr.wisc.edu>
2013-03-25 12:08:17 -0500

    added the nodelinetonode data functions which returns the current line string as a struct

--- mesh_funcs.cpp
+++ mesh_funcs.cpp
@@ -18,17 +18,29 @@ int LoadMeshFile(std::string meshfile, std::vector<node_struct> &node_data
   std::ifstream mesh_file; // the abaqus mesh
   std::string line_read; // the line being read from file
 
+  node_struct tmp_node_data; // temporary copy of the current lines node data
+
   std::cout << "Reading abaqus mesh " << meshfile << std::endl;
   
   mesh_file.open(meshfile.c_str(), ios::in); // open the mesh file
   
   if( mesh_file.is_open() ) // while we can read data
     {
+      int set_allnodes = 0 ; // when file is reading the line NSET=ALLNODES
       while( getline(mesh_file,line_read) ) //take the current line and decide what do 
 	{
-	  
+	  if ( set_allnodes == 1 )
+	    {
+	      tmp_node_data = NodeLineToNodeData(line_read); // convert the line being read into node struct
+	    }
+
+	  if (std::string::npos != line_read.find("NSET=ALLNODES"))
+	    {
+	      std::cout << line_read << std::endl;
+	      set_allnodes = 1;
+	    }
+
 	}
-      // do stuff
     }
   else
     {
@@ -39,3 +51,16 @@ int LoadMeshFile(std::string meshfile, std::vector<node_struct> &node_data
 
   return 0;
 }
+
+/*
+ *
+ */
+node_struct NodeLineToNodeData(std::string line_read)
+{
+  node_struct mesh_node_structure;
+
+  std::cout << "ey up" << std::endl;
+
+  return mesh_node_structure;
+}
+
