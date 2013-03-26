8bfa819f3f4cab85c0df5e9547de2477d528e7bb
makeclean <davisa@engr.wisc.edu>
2013-03-26 10:23:45 -0500

    added fancier cuda query options

--- cuda_query.cpp
+++ cuda_query.cpp
@@ -9,5 +9,29 @@ int CudaQuery()
   int deviceCount = 0;
   cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
   std::cout << "There are " << deviceCount << " devices" << std::endl;
+
+  int dev, driverVersion = 0, runtimeVersion = 0;
+  for (dev = 0; dev < deviceCount; ++dev)
+    {
+        cudaSetDevice(dev);
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+        printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
+
+        // Console log
+        cudaDriverGetVersion(&driverVersion);
+        cudaRuntimeGetVersion(&runtimeVersion);
+        printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n", driverVersion/1000, (driverVersion%100)/10, runtimeVersion/1000, (runtimeVersion%100)/10);
+        printf("  CUDA Capability Major/Minor version number:    %d.%d\n", deviceProp.major, deviceProp.minor);
+
+        char msg[256];
+        sprintf(msg, "  Total amount of global memory:                 %.0f MBytes (%llu bytes)\n",
+                (float)deviceProp.totalGlobalMem/1048576.0f, (unsigned long long) deviceProp.totalGlobalMem);
+        printf("%s", msg);
+
+        printf("  GPU Clock rate:                                %.0f MHz (%0.2f GHz)\n", deviceProp.clockRate * 1e-3f, deviceProp.clockRate * 1e-6f);
+    }
+
   return 0;
 }
