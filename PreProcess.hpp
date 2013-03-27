629ea2086e9b78dae60a0fab71e2ef3300a84179
Andrew Davis <davisa@engr.wisc.edu>
2013-03-26 19:49:41 -0500

    added tet and volume reordering routines

--- PreProcess.hpp
+++ PreProcess.hpp
@@ -2,3 +2,13 @@
  * preproccessing
  */
 int PreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data);
+
+/*
+ *  ReNumberTets
+ */ 
+int ReNumberTets(std::vector<tet_struct> &tet_data); 
+
+/*
+ *  ReNumberNodes
+ */ 
+int ReNumberNodes(std::vector<node_struct> &node_data); 
