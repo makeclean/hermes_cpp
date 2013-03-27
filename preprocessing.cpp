c110c95127480b7b8dbcf2c57ef8315f276204f0
Andrew Davis <davisa@engr.wisc.edu>
2013-03-26 20:32:57 -0500

    modifed the function to provide functions to renumber node and tets

--- preprocessing.cpp
+++ preprocessing.cpp
@@ -13,13 +13,15 @@
 
 int PreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data)
 {
+  std::vector<node_struct> copy_original_node; // copy of node data
+  
   int errorcode; // return error code from functions
   
   std::cout << "Performing preprocessing" << std::endl;
   
-  errorcode = ReNumberNodes(node_data); // renumber the nodes from 1 to N
+  errorcode = ReNumberNodes(node_data, copy_original_node); // renumber the nodes from 1 to N
   
-  errorcode = ReNumberTets(tet_data); // renumber the nodes from 1 to M
+  errorcode = ReNumberTets(copy_original_node,tet_data); // renumber the nodes from 1 to M
   
   std::cout << "Calculating volumes " << std::endl;
   
@@ -27,14 +29,20 @@ int PreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet
 }
 
 /*
- * 	renumber the nodes from 0 to N-1
+ * 	renumber the nodes from 0 to N-1, i.e. makes the node list self referential 
  */
-int ReNumberNodes(std::vector<node_struct> &node_data)
+int ReNumberNodes(std::vector<node_struct> &node_data, std::vector<node_struct> &original_nodes)
 {
+    int num_node = 0; // node num index
     std::vector<node_struct>::iterator node_it; //iterator through node_data
+    node_struct copy_of_nodes; // structure template
+    
     for ( node_it = node_data.begin() ; node_it != node_data.end() ; ++node_it )
       {
-	std::cout << (node_it->node_number) << std::endl;
+	copy_of_nodes.node_number = node_data[num_node].node_number; // copy the old node number
+	original_nodes.push_back(copy_of_nodes); // push the original data to vector
+	node_data[num_node].node_number = num_node; 
+	num_node++;
       }
       
     return 0;
@@ -43,13 +51,37 @@ int ReNumberNodes(std::vector<node_struct> &node_data)
 /*
  * 	renumber the tets from 0 to N-1
  */
-int ReNumberTets(std::vector<tet_struct> &tet_data)
+int ReNumberTets(std::vector<node_struct> node_data, std::vector<tet_struct> &tet_data)
 {
     std::vector<tet_struct>::iterator tet_it; //iterator through tet_data
-    for ( tet_it = tet_data.begin() ; tet_it != tet_data.end() ; ++tet_it )
+    std::vector<node_struct>::iterator node_it;
+    
+    int node_num = 0;
+    int tet_num = 0;
+    
+    for ( node_it = node_data.begin() ; node_it != node_data.end() ; ++node_it )
       {
-	std::cout << (tet_it->tet_num) << std::endl;
-      }
-      
+	for ( tet_it = tet_data.begin() ; tet_it != tet_data.end() ; ++tet_it )
+	  {
+	    if ( ( tet_it->link1 ) == ( node_it->node_number ) )
+	      tet_it->link1 = node_num; 
+	    if ( ( tet_it->link2 ) == ( node_it->node_number ) )
+	      tet_it->link2 = node_num; 
+	    if ( ( tet_it->link3 ) == ( node_it->node_number ) )
+	      tet_it->link3 = node_num; 
+	    if ( ( tet_it->link4 ) == ( node_it->node_number ) )
+	      tet_it->link4 = node_num;	    
+	  }
+	  
+	node_num++;
+      } 
+     
+     for ( tet_it = tet_data.begin() ; tet_it != tet_data.end() ; ++tet_it )
+       {
+	     tet_it->tet_num = tet_num;
+	     tet_num++;
+	 
+       }
+     
     return 0;
 }
