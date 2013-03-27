f0b0f222d2b038405b96857961321ce514349b0c
Andrew Davis <davisa@engr.wisc.edu>
2013-03-26 19:50:04 -0500

    added skeleton functions to renumber nodes and vols

--- preprocessing.cpp
+++ preprocessing.cpp
@@ -1,13 +1,55 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <vector>
-#include <stdlib.h>
 
 #include "MeshData.hpp"
 #include "PreProcess.hpp"
 
+/*
+ * 	Function to preprocess the mesh to reorder the element from 1 to N, 
+ * 	to calculate the volume of the tets 
+ * 	and the adjacancy information
+ */
+
 int PreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data)
 {
+  int errorcode; // return error code from functions
+  
+  std::cout << "Performing preprocessing" << std::endl;
+  
+  errorcode = ReNumberNodes(node_data); // renumber the nodes from 1 to N
+  
+  errorcode = ReNumberTets(tet_data); // renumber the nodes from 1 to M
+  
+  std::cout << "Calculating volumes " << std::endl;
+  
   return 0;
+}
+
+/*
+ * 	renumber the nodes from 0 to N-1
+ */
+int ReNumberNodes(std::vector<node_struct> &node_data)
+{
+    std::vector<node_struct>::iterator node_it; //iterator through node_data
+    for ( node_it = node_data.begin() ; node_it != node_data.end() ; ++node_it )
+      {
+	std::cout << (node_it->node_number) << std::endl;
+      }
+      
+    return 0;
+}
+
+/*
+ * 	renumber the tets from 0 to N-1
+ */
+int ReNumberTets(std::vector<tet_struct> &tet_data)
+{
+    std::vector<tet_struct>::iterator tet_it; //iterator through tet_data
+    for ( tet_it = tet_data.begin() ; tet_it != tet_data.end() ; ++tet_it )
+      {
+	std::cout << (tet_it->tet_num) << std::endl;
+      }
+      
+    return 0;
 }
