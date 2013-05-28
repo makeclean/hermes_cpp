06c9e3fa49062e02168c5bdffc9eae05b15e6091
makeclean <davisa@engr.wisc.edu>
2013-05-27 21:27:13 -0500

    added function definition CalculateVolume

--- PreProcess.hpp
+++ PreProcess.hpp
@@ -2,3 +2,4 @@
  * preproccessing
  */
 int PreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data);
+int CalculateVolume(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data);
