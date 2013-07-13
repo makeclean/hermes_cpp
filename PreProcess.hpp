96aba5e750e05d39498ba3d70cedf87b308934b7
makeclean <davisa@engr.wisc.edu>
2013-07-13 18:46:39 -0500

    added functions for adjacancy calc

--- PreProcess.hpp
+++ PreProcess.hpp
@@ -3,3 +3,7 @@
  */
 int PreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data);
 int CalculateVolume(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data);
+int shared(int node_test, int index, std::vector<tet_struct> tet_data);
+int DetermineAdjacancy(std::vector<node_struct> node_data,std::vector<tet_struct>tet_data);
+
+int CudaPreProcess(std::vector<node_struct> &node_data, std::vector<tet_struct> &tet_data);
