69444913818ff4ec5450e0f8d2872e7f4fdc9878
makeclean <davisa@engr.wisc.edu>
2013-07-13 18:46:28 -0500

    added call

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector> 
 
+#include <time.h>       // for timing the routine
+
 #include "MeshData.hpp"
 #include "PreProcess.hpp"
 #include "CudaData.hpp"
@@ -13,6 +15,8 @@ int main ( int argc, char *argv[] )
   int num_vols; // number of volumes
   std::vector<tet_struct> tet_data;
   std::vector<node_struct> node_data;
+
+  time_t time_before,time_after; // for timing routines
 
   // parse the command line arguments
   for ( int argnum = 0 ; argnum <= argc-1 ; argnum++ )
@@ -33,15 +37,26 @@ int main ( int argc, char *argv[] )
 	}
 
     }
-
+ 
   errorcode = CudaQuery(); // query the cuda devices
   if ( errorcode == 0 )
     {
       std::cerr << "There were no CUDA capable devices found" << std::endl;
       std::cerr << "Consider running in MPI or OMP mode" << std::endl;
     }
-
+  time(&time_before); 
+  std::cout << "Preprocess" << std::endl;
   errorcode = PreProcess(node_data,tet_data); // Pre process the mesh data, renumber etc
+  time(&time_after); 
+  double seconds = difftime(time_after,time_before); //get the time in seconds
+  std::cout << "serial version took " << seconds << " s" << std::endl;
+
+  time(&time_before); 
+  std::cout << "CudaPreprocess" << std::endl;
+  errorcode = CudaPreProcess(node_data,tet_data); // GPU Pre process the mesh data, renumber 
+  time(&time_after); 
+  seconds = difftime(time_after,time_before); //get the time in seconds
+  std::cout << "gpu version took " << seconds << " s" << std::endl;
 
   return 0;
 }
